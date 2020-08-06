f76d9041a766a8404e330ec1d706cefd545ddf72
vuchuc781999 <vuchuc781999@gmail.com>
2020-08-06 10:16:10 +0700

    refs task #26407 database migration

---
+++ database/migrations/2020_08_06_020451_create_contents_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('contentable_id');
+            $table->string('contentable_type');
+            $table->longText('content');
+            $table->integer('version');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+}
